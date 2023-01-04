36f37073066416f04e025acf619fdc92fae7eb46
SwiftbyDrew <116314715+SwiftbyDrew@users.noreply.github.com>
2023-01-04 18:55:02 -0300

    Remove bcryptprovider constructor

--- src/createUser/providers/Bcrypt/BcryptProvider.ts
+++ src/createUser/providers/Bcrypt/BcryptProvider.ts
@@ -2,8 +2,6 @@ import bcrypt from 'bcrypt'
 import { BcryptProviderInterface } from './BcryptProviderInterface';
 
 export class BcryptProvider implements BcryptProviderInterface {
-    constructor() {}
-
     async cryptPassword(password: string): Promise<string> {
         const salt = await bcrypt.genSalt(11);
         const hash = bcrypt.hashSync(password, salt);
