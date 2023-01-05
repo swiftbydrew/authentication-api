f0c2e7a15eadff01698cc7e988487124b126300c
Andrew Castro <swiftbydrew@gmail.com>
2023-01-05 15:30:16 -0300

    Add userId in request and set auth middleware userId

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken'
 
+interface TokenPayload {
+    id: string;
+    iat: number;
+    exp: number;
+}
+
 export default function authMiddleware(req: Request, res: Response, next: NextFunction) {
     const { authorization } = req.headers;
 
@@ -11,7 +17,11 @@ export default function authMiddleware(req: Request, res: Response, next: NextFu
     const token = authorization.replace('Bearer', '').trim();
     try {
         const data = jwt.verify(token, 'secret')
-        return data
+        const { id } = data as TokenPayload;
+
+        req.userId = id;
+
+        return next();
     } catch {
         return res.sendStatus(401);
     }
