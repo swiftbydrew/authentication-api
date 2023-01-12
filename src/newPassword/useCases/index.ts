80c2e03788bd4d06cc68da5981b5f8b377937f26
Andrew Castro <swiftbydrew@gmail.com>
2023-01-12 17:15:39 -0300

    Adding bcrypt dependency injection

--- src/newPassword/useCases/index.ts
+++ src/newPassword/useCases/index.ts
@@ -1,8 +1,10 @@
+import { BcryptProvider } from "../../provider/Bcrypt/BcryptProvider"
 import { NewPasswordRepositoryImplementation } from "../repositories/implementation/NewPasswordRepositoryImplementation"
 import { NewPasswordController } from "./NewPasswordController"
 import { NewPasswordUseCase } from "./NewPasswordUseCase"
 
-const newPasswordRepositoryImplementation = new NewPasswordRepositoryImplementation()
+const bcrypt = new BcryptProvider()
+const newPasswordRepositoryImplementation = new NewPasswordRepositoryImplementation(bcrypt)
 const newPasswordUseCase = new NewPasswordUseCase(newPasswordRepositoryImplementation)
 const newPasswordController = new NewPasswordController(newPasswordUseCase)
 
